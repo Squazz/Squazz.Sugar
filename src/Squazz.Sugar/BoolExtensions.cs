439bb49b11d1fc998479f4bafc8c39c675bcc997
Squazz <GitHub@squazz.dk>
2022-03-17 14:27:06 +0100

    Added CountIsAtLeast & CountIsExactly

--- src/Squazz.Sugar/BoolExtensions.cs
+++ src/Squazz.Sugar/BoolExtensions.cs
@@ -13,5 +13,22 @@ namespace Squazz.Sugar
 
             return !source.Any();
         }
+
+        public static bool CountIsAtLeast<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            return source.Skip(count - 1).Any();
+        }
+
+        public static bool CountIsExactly<TSource>(this IEnumerable<TSource> source, int count)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            var enumerable = source.ToArray();
+            return enumerable.CountIsAtLeast(count) && enumerable.Skip(count).None();
+        }
     }
 }
