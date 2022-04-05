e0176c0affe60757e889340147d1452b94028b05
Squazz <GitHub@squazz.dk>
2022-04-05 11:39:27 +0200

    Adding first DateHelperMethods

---
+++ src/Squazz.Sugar/DateHelperMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Squazz.Sugar
+{
+    public static class DateHelperMethods
+    {
+        public static bool DoesDateRangesOverlap(DateTime startRangeA, DateTime endRangeA, DateTime startRangeB, DateTime endRangeB)
+        {
+            // Based on logic from this StackOverflow answer: https://stackoverflow.com/a/325964
+
+            return startRangeA <= endRangeB
+                   && endRangeA >= startRangeB;
+        }
+
+        public static bool DoesDateRangesOverlap(DateTimeOffset startRangeA, DateTimeOffset endRangeA, DateTimeOffset startRangeB, DateTimeOffset endRangeB)
+        {
+            // Based on logic from this StackOverflow answer: https://stackoverflow.com/a/325964
+
+            return startRangeA <= endRangeB
+                   && endRangeA >= startRangeB;
+        }
+    }
+}
